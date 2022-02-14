1ee0b256a3132942a53e05998753e910fd742c4c
flllck <sadalexander11@gmail.com>
2022-02-14 15:04:02 +0300

    Added: validates and scope methods for models

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,14 +3,25 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: 'User'
 
   has_many :test_passages
+  has_many :questions, dependent: :destroy
   has_many :users, through: :test_passages, dependent: :destroy
 
+  validates :title, presence: true, uniqueness: { scope: :level }
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def self.sort_by_categories(category)
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+  scope :sort_by_category, lambda { |category|
+    joins(:category)
+      .where(categories: { title: category })
+      .order(title: :desc)
+  }
+
+  def self.sort_by_category_array(category)
     joins(:category)
       .where(categories: { title: category })
       .order(title: :desc)
       .pluck(:title)
   end
 end
-
