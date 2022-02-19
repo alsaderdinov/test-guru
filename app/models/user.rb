1de8f95d26020f39fcaa23cbcb53d925e5094642
flllck <sadalexander11@gmail.com>
2022-02-19 15:19:09 +0300

    Added: auth with devise and refactoring some code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,17 @@
 class User < ApplicationRecord
-
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-  before_save { self.email = email.downcase }
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :confirmable
 
   has_many :test_passages, dependent: :destroy
   has_many :authored_tests, class_name: 'Test', foreign_key: :author_id
   has_many :tests, through: :test_passages
 
-  has_secure_password
   validates :name, presence: true
-  validates :email, format: { with: VALID_EMAIL_REGEX },
-    uniqueness: true
 
   def tests_by_level(level)
     tests.where(level: level)
@@ -20,6 +20,4 @@ class User < ApplicationRecord
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
   end
-
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-
   root 'tests#index'
 
-  get :signup, to: 'users#new'
-  get :login, to: 'sessions#new'
-  delete :logout, to: 'sessions#destroy'
-
-  resources :users, only: :create
-  resources :sessions, only: :create
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
 
   resources :tests do
     resources :questions, shallow: true, except: :index do
@@ -15,12 +9,9 @@ Rails.application.routes.draw do
     end
 
     post :start, on: :member
-
   end
 
   resources :test_passages, only: %i[show update] do
     get :result, on: :member
   end
-
 end
-
