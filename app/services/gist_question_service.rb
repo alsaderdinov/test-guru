295e200f22769762dd7724504e7f9fb6dd6addd8
flllck <sadalexander11@gmail.com>
2022-02-21 21:30:05 +0300

    Added: Github octokit client in gist_question_service

---
+++ app/services/gist_question_service.rb
@@ -0,0 +1,34 @@
+class GistQuestionService
+  def initialize(question, client = default_client)
+    @question = question
+    @test = @question.test
+    @client = client
+  end
+
+  def call
+    @client.create_gist(gist_params)
+  end
+
+  private
+
+  def gist_params
+    {
+      description: "A question about#{@test.title} from TestGuru",
+      files: {
+        'test-guru-description.txt' => {
+          content: gist_content
+        }
+      }
+    }
+  end
+
+  def gist_content
+    content = [@question.body]
+    content += @question.answers.pluck(:body)
+    content.join("\n")
+  end
+
+  def default_client
+    Octokit::Client.new(access_token: ENV['ACCESS_TOKEN'])
+  end
+end
