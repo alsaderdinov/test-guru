2086d1f8b6cfde36189f1a76847e30ede9227926
flllck <sadalexander11@gmail.com>
2022-02-27 14:36:48 +0300

    Fixed: returned an mistaked deleted import

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import "channels"
 
 require ('utilities/sorting')
 require('utilities/password_match')
