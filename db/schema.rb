1e3bfa23b4188cd2e3701337d7faa2b0977bac98
flllck <sadalexander11@gmail.com>
2022-03-11 12:12:36 +0300

    Added: rule_param to badge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_07_094935) do
+ActiveRecord::Schema.define(version: 2022_03_11_085550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2022_03_07_094935) do
     t.integer "rule"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "rule_param"
     t.index ["rule"], name: "index_badges_on_rule"
   end
 
